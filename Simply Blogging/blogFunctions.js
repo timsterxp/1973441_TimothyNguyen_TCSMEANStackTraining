b10d25c4f9f17d215af65517b1b94d414366a206
Tim Nguyen <timmyn97@gmail.com>
2021-03-17 01:00:11 -0700

    Adding initial localStorage functionality

--- Simply Blogging/blogFunctions.js
+++ Simply Blogging/blogFunctions.js
@@ -1,3 +1,4 @@
+var allBlogs = [];
 function addBlog(){
     var title = document.getElementById("title").value;
     var desc = document.getElementById("desc").value;
@@ -5,8 +6,39 @@ function addBlog(){
     console.log(title)
     console.log(desc);
     console.log(imageInfo);
+    storeObject(title,desc,imageInfo);z
     document.getElementById("titleInfo").innerHTML=title;
     document.getElementById("descInfo").innerHTML=desc;
     document.getElementById("imageInfo").src=imageInfo;
 }
 
+function loadPreviousBlogs(){
+    var obj = JSON.parse(localStorage.getItem("blogs"));
+    obj.forEach(function(previousBlogs){
+        allBlogs.push(previousItems);
+    });
+}
+
+function storeObject{title,desc,imageInfo}{
+    
+    var obj = {}
+    obj.title=title;
+    obj.desc=desc;
+    obj.info=imageInfo;
+    return obj;
+}
+
+//Example Function from DomOperations.html
+//To be revised
+function showNewBlog(){
+    var table = document.getElementById("employeeList")
+    var body = table.getElementsByTagName("tbody")[0];
+    var newRow = body.insertRow(body.length);  // row created 
+   
+    var cell1 = newRow.insertCell(0);          // cell created 
+    cell1.innerHTML=data.name;                 // value placed 
+   
+    var cell2 = newRow.insertCell(1);          // cell created 
+    cell2.innerHTML=data.age;                 // value placed
+   
+}
