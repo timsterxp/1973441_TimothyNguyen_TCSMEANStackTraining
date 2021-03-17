b1fb2af042e8f1ff1f07176baf6c0b756082d46d
Tim Nguyen <timmyn97@gmail.com>
2021-03-17 13:38:55 -0700

    Finalizing localStorage, and a lot of CSS styling changes

--- Simply Blogging/blogFunctions.js
+++ Simply Blogging/blogFunctions.js
@@ -1,44 +1,53 @@
 var allBlogs = [];
-function addBlog(){
+//var count = 0;
+
+function addBlog() {
     var title = document.getElementById("title").value;
-    var desc = document.getElementById("desc").value;
+    var desc = document.getElementById("description").value;
+
     var imageInfo = document.getElementById("imageId").files[0].name;
-    console.log(title)
-    console.log(desc);
-    console.log(imageInfo);
-    storeObject(title,desc,imageInfo);z
-    document.getElementById("titleInfo").innerHTML=title;
-    document.getElementById("descInfo").innerHTML=desc;
-    document.getElementById("imageInfo").src=imageInfo;
+    count++;
+    localStorage.setItem("numBlogs", count);
+    storeObject(title, desc, imageInfo);
+
+
+    resetData();
+
 }
 
-function loadPreviousBlogs(){
+function resetData() {
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("imageId").value = "";
+}
+
+function loadPreviousBlogs() {
     var obj = JSON.parse(localStorage.getItem("blogs"));
-    obj.forEach(function(previousBlogs){
-        allBlogs.push(previousItems);
+    var counter = 0;
+    obj.forEach(function(previousBlogs) {
+        allBlogs.push(previousBlogs);
+        var gridLayout = document.getElementById("insertHere");
+        var col = "<div class = 'col-sm myBlogs'><div class ='col-sm-offset-12'>"
+        col += "<h1 class='display-2 text'>" + previousBlogs.title + "</h1></br>"
+        col += "<p class='lead text2'>" + previousBlogs.desc + "</p></br>"
+        col += "<p><img src =\"" + previousBlogs.info + "\" class='images'/>"
+        col += "</div></div>"
+        counter++;
+        gridLayout.innerHTML += col;
+        if (counter % 3 == 0) {
+            gridLayout.innerHTML += "</div> <div class= 'row' >"
+        }
     });
+    //count = JSON.parse(localStorage.getItem("numBlogs"));
+
 }
 
-function storeObject{title,desc,imageInfo}{
-    
+function storeObject(title, desc, imageInfo) {
+
     var obj = {}
-    obj.title=title;
-    obj.desc=desc;
-    obj.info=imageInfo;
-    return obj;
-}
-
-//Example Function from DomOperations.html
-//To be revised
-function showNewBlog(){
-    var table = document.getElementById("employeeList")
-    var body = table.getElementsByTagName("tbody")[0];
-    var newRow = body.insertRow(body.length);  // row created 
-   
-    var cell1 = newRow.insertCell(0);          // cell created 
-    cell1.innerHTML=data.name;                 // value placed 
-   
-    var cell2 = newRow.insertCell(1);          // cell created 
-    cell2.innerHTML=data.age;                 // value placed
-   
+    obj.title = title;
+    obj.desc = desc;
+    obj.info = imageInfo;
+    allBlogs.push(obj);
+    localStorage.setItem("blogs", JSON.stringify(allBlogs));
 }
