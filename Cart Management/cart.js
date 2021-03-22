be7c45b32ef83beb267ed85c150d152fb15c1c7a
Tim Nguyen <timmyn97@gmail.com>
2021-03-22 14:38:02 -0700

    Adding table functionality + minor fixes to main page

--- Cart Management/cart.js
+++ Cart Management/cart.js
@@ -1,58 +1,129 @@
 var cartSize = 0;
+var cartTotalPrice = 0;
 var cartItem = /** @class */ (function() {
-    function cartItem(name, price) {
+    function cartItem(name, price, id) {
+        this.id = id;
         this.name = name;
         this.price = price;
     }
     return cartItem;
 }());
+
+function populateTable() {
+    var currentArray = JSON.parse(localStorage.getItem("shoppingCartItems"));
+    var tableContents = document.getElementById("insertCart");
+    for (var _i = 0, currentArray_1 = currentArray; _i < currentArray_1.length; _i++) {
+        var item = currentArray_1[_i];
+        tableContents.innerHTML += "<tr><th scope='row'>" + item.name + "</th><th>" + item.price + "</th></tr>";
+        // <th scope="row">itemName</tr>
+        cartTotalPrice = +cartTotalPrice + +item.price;
+    }
+    var subTotal = document.getElementById("subtotal");
+    cartTotalPrice.toLocaleString();
+    subTotal.innerHTML = "Your Total Price Is: $" + cartTotalPrice;
+}
+
+function updateCartSize() {
+    cartSize++;
+    var cartTotal = document.getElementById("cartSize");
+    cartTotal.innerHTML = 'Cart Size: ' + cartSize;
+}
+
+function checkAdded() {
+    var prevArray = JSON.parse(localStorage.getItem("shoppingCartItems"));
+    if (prevArray == null) {
+        return;
+    } else {
+        shoppingCart = prevArray;
+    }
+    for (var _i = 0, prevArray_1 = prevArray; _i < prevArray_1.length; _i++) {
+        var item = prevArray_1[_i];
+        if (item.id == 1) {
+            var button = document.getElementById("itemOneAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+        if (item.id == 2) {
+            var button = document.getElementById("itemTwoAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+        if (item.id == 3) {
+            var button = document.getElementById("itemThreeAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+        if (item.id == 4) {
+            var button = document.getElementById("itemFourAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+        if (item.id == 5) {
+            var button = document.getElementById("itemFiveAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+        if (item.id == 6) {
+            var button = document.getElementById("itemSixAdd");
+            button.disabled = true;
+            button.value = "Sold Out";
+            updateCartSize();
+        }
+    }
+}
 var shoppingCart = new Array();
 
-function addObject(itemName, itemPrice) {
-    var newItem = new cartItem(itemName, itemPrice);
+function addObject(itemName, itemPrice, itemId) {
+    var newItem = new cartItem(itemName, itemPrice, itemId);
     console.log(newItem);
     shoppingCart.push(newItem);
     localStorage.setItem("shoppingCartItems", JSON.stringify(shoppingCart));
+    updateCartSize();
 }
 
 function addItemOne() {
     var itemName = document.getElementById("itemOne").textContent;
     var itemPrice = parseInt(document.getElementById("itemOnePrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " " + itemPrice);
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 1);
 }
 
 function addItemTwo() {
     var itemName = document.getElementById("itemTwo").textContent;
     var itemPrice = parseInt(document.getElementById("itemTwoPrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " ");
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 2);
 }
 
 function addItemThree() {
     var itemName = document.getElementById("itemThree").textContent;
     var itemPrice = parseInt(document.getElementById("itemThreePrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " ");
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 3);
 }
 
 function addItemFour() {
     var itemName = document.getElementById("itemFour").textContent;
     var itemPrice = parseInt(document.getElementById("itemFourPrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " ");
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 4);
 }
 
 function addItemFive() {
     var itemName = document.getElementById("itemFive").textContent;
     var itemPrice = parseInt(document.getElementById("itemFivePrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " ");
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 5);
 }
 
 function addItemSix() {
     var itemName = document.getElementById("itemSix").textContent;
     var itemPrice = parseInt(document.getElementById("itemSixPrice").textContent.replace('$', ""));
     console.log("Added" + itemName + " ");
-    addObject(itemName, itemPrice);
+    addObject(itemName, itemPrice, 6);
 }
--- Cart Management/cart.ts
+++ Cart Management/cart.ts
@@ -1,33 +1,107 @@
 let cartSize=0;
-
+let cartTotalPrice=0;
 class  cartItem{
     name:string;
     price:number;
-    constructor(name:string,price:number){
+    id:number;
+    constructor(name:string,price:number,id:number){
+        this.id=id;
         this.name=name;
         this.price=price;
     }
 
 }
 
+function populateTable(){
+    let currentArray = JSON.parse(localStorage.getItem("shoppingCartItems"));
+    let tableContents = document.getElementById("insertCart");
+    for (let item of currentArray){
+        tableContents.innerHTML+="<tr><th scope='row'>"+item.name+"</th><th>"+item.price+"</th></tr>";
+        // <th scope="row">itemName</tr>
+        cartTotalPrice= +cartTotalPrice+ +item.price;
+    }
+
+    let subTotal= document.getElementById("subtotal");
+    cartTotalPrice.toLocaleString();
+    subTotal.innerHTML= "Your Total Price Is: $"+cartTotalPrice;
+   
+
+}
+function updateCartSize(){
+    cartSize++;
+    let cartTotal = document.getElementById("cartSize");
+    cartTotal.innerHTML='Cart Size: '+ cartSize;
+}
+
 function checkAdded(){
-    
+    let prevArray = JSON.parse(localStorage.getItem("shoppingCartItems"));
+    if (prevArray==null){
+        return;
+    }else {
+        shoppingCart=prevArray;
+    }
+    for (let item of prevArray){
+        if (item.id==1){
+            let button = (document.getElementById("itemOneAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+            updateCartSize();
+        }
+        if (item.id==2){
+            let button = (document.getElementById("itemTwoAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+
+            updateCartSize();
+        }
+        if (item.id==3){
+            let button = (document.getElementById("itemThreeAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+
+            updateCartSize();
+        }
+        if (item.id==4){
+            let button = (document.getElementById("itemFourAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+
+            updateCartSize();
+        }
+        if (item.id==5){
+            let button = (document.getElementById("itemFiveAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+  
+            updateCartSize();
+        }
+        if (item.id==6){
+            let button = (document.getElementById("itemSixAdd") as HTMLInputElement);
+            button.disabled=true;
+            button.value="Sold Out";
+
+            updateCartSize();
+        }
+    }
+   
 }
 
 let shoppingCart = new Array<cartItem>();
 
-function addObject(itemName:string,itemPrice:number){
-    let newItem=new cartItem(itemName,itemPrice);
+function addObject(itemName:string,itemPrice:number,itemId:number){
+    let newItem=new cartItem(itemName,itemPrice,itemId);
     console.log(newItem);
     shoppingCart.push(newItem);
     localStorage.setItem("shoppingCartItems",JSON.stringify(shoppingCart));
+    updateCartSize();
 }
 
 function addItemOne(){
     let itemName = document.getElementById("itemOne").textContent;
     let itemPrice=parseInt(document.getElementById("itemOnePrice").textContent.replace('$',""));
     console.log("Added" + itemName + " "+itemPrice);
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,1);
+
 
 }
 
@@ -35,33 +109,33 @@ function addItemTwo(){
     let itemName=document.getElementById("itemTwo").textContent;
    let itemPrice=parseInt(document.getElementById("itemTwoPrice").textContent.replace('$',""));
     console.log("Added" + itemName + " ");
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,2);
 }
 
 function addItemThree(){
     let itemName=document.getElementById("itemThree").textContent;
     let itemPrice=parseInt(document.getElementById("itemThreePrice").textContent.replace('$',""));
     console.log("Added" + itemName + " ");
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,3);
 }
 
 function addItemFour(){
     let itemName=document.getElementById("itemFour").textContent;
     let itemPrice=parseInt(document.getElementById("itemFourPrice").textContent.replace('$',""));
     console.log("Added" + itemName + " ");
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,4);
 }
 
 function addItemFive(){
     let itemName=document.getElementById("itemFive").textContent;
     let itemPrice=parseInt(document.getElementById("itemFivePrice").textContent.replace('$',""));
     console.log("Added" + itemName + " ");
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,5);
 }
 
 function addItemSix(){
     let itemName=document.getElementById("itemSix").textContent;
     let itemPrice=parseInt(document.getElementById("itemSixPrice").textContent.replace('$',""));
     console.log("Added" + itemName + " ");
-    addObject(itemName,itemPrice);
+    addObject(itemName,itemPrice,6);
 }
