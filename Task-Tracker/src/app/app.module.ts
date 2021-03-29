b3d12add4c53b2eb417ddbf071c907888deda476
Tim Nguyen <timmyn97@gmail.com>
2021-03-29 11:33:35 -0700

    Add imports for PWA

--- Task-Tracker/src/app/app.module.ts
+++ Task-Tracker/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TaskPageComponent } from './task-page/task-page.component';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { TaskPageComponent } from './task-page/task-page.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Task-Tracker/src/app/task.service.ts
+++ Task-Tracker/src/app/task.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+
+  storeTask(task:any){
+    this.http.post("http://localhost:3000/taskList",task).subscribe(result=>console.log(result));
+  }
 }
