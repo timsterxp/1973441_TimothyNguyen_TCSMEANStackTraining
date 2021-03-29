6ef97ea730b3c65c9135a6f4a1ffb213bbf7727f
Tim Nguyen <timmyn97@gmail.com>
2021-03-29 14:47:17 -0700

    Reverting to an earlier status

--- Task-Tracker/src/app/task.service.ts
+++ Task-Tracker/src/app/task.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from './task.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,4 +12,10 @@ export class TaskService {
   storeTask(task:any){
     this.http.post("http://localhost:3000/taskList",task).subscribe(result=>console.log(result));
   }
+
+  loadTaskDetails():Observable<Task[]>{
+    return this.http.get<Task[]>("/assets/task.json");
+  }
+
+  
 }
