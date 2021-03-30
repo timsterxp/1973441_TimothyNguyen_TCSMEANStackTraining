ee79849a8cf6c8d073029ac299e34deaefa9ebdc
Tim Nguyen <timmyn97@gmail.com>
2021-03-30 13:26:36 -0700

    Final changes to question.json; styling fixes

--- Online-Test-Application/src/app/question-retrieve/question-retrieve.component.ts
+++ Online-Test-Application/src/app/question-retrieve/question-retrieve.component.ts
@@ -23,33 +23,41 @@ export class QuestionRetrieveComponent implements OnInit {
     this.questionService.loadTestQuestions().subscribe(result=>this.questions=result)
   }
 
+//==================================================
+// When Clicking Submit, Check Answers For Score
+//==================================================
   checkQuestion(){
+    //Variables to keep track of score
     let numOfQuestions=0;
     let correctNum=0;
+
+    //Iterate through the questions array which already contains another array
     for (var question of this.questions){
       numOfQuestions++;
      for (var chosen of question.choices){
+       //Retrieve button by id
        let button=document.getElementById(chosen.id.toString()) as HTMLInputElement;
-       button.disabled=true;
-       if (button!=null)
-       if (button.checked){
+       button.disabled=true; //Disable all input when showing answers
+       if (button!=null) //Check if Button actually was retrieved
+       if (button.checked){ //Check if button is currently the chosen answer
+
+        //If correct answer then display "Correct Answer"
          if (question.answer==chosen.choice){
-          button.className="btn-check btn-primary";
-          button.style.borderColor="green";
           let textAnswer=document.getElementById('insertHere'+question.questionId);
-          textAnswer.innerHTML+="Right answer";
-          console.log("Correct answer!");
+          textAnswer.innerHTML+="Correct answer!";
           correctNum++;
          }else {
+
+          //If wrong, display what they answered and the correct answer
           let textAnswer=document.getElementById(question.questionId+'insertHere');
-          textAnswer.innerHTML+="Wrong Answer! The correct answer is " + question.answer ;
-          console.log("Wrong Answer!");
+          textAnswer.innerHTML+="Wrong Answer! The correct answer is " + question.answer + " but you answered "+ chosen.choice;
          }
      
        }
      }
     }
 
+    //Calculate Score and Print correct message
     let score=(correctNum/numOfQuestions)*100;
     let scoreText=document.getElementById('scoreHere');
     if (score>=80){
