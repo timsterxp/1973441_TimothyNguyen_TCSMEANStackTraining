9e6c7d48aeb88336b5103ba4fdaecf8d1f4db8ff
Tim Nguyen <timmyn97@gmail.com>
2021-03-30 12:38:51 -0700

    Add score functionality + correct/wrong answers

--- Online-Test-Application/src/app/question-retrieve/question-retrieve.component.ts
+++ Online-Test-Application/src/app/question-retrieve/question-retrieve.component.ts
@@ -24,16 +24,41 @@ export class QuestionRetrieveComponent implements OnInit {
   }
 
   checkQuestion(){
-    let button=document.getElementById("101") as HTMLInputElement;
-    if (button.checked)
-    console.log("You entered ");
+    let numOfQuestions=0;
+    let correctNum=0;
+    for (var question of this.questions){
+      numOfQuestions++;
+     for (var chosen of question.choices){
+       let button=document.getElementById(chosen.id.toString()) as HTMLInputElement;
+       button.disabled=true;
+       if (button!=null)
+       if (button.checked){
+         if (question.answer==chosen.choice){
+          button.className="btn-check btn-primary";
+          button.style.borderColor="green";
+          let textAnswer=document.getElementById('insertHere'+question.questionId);
+          textAnswer.innerHTML+="Right answer";
+          console.log("Correct answer!");
+          correctNum++;
+         }else {
+          let textAnswer=document.getElementById(question.questionId+'insertHere');
+          textAnswer.innerHTML+="Wrong Answer! The correct answer is " + question.answer ;
+          console.log("Wrong Answer!");
+         }
+     
+       }
+     }
+    }
 
-
+    let score=(correctNum/numOfQuestions)*100;
+    let scoreText=document.getElementById('scoreHere');
+    if (score>=80){
+      scoreText.innerHTML+="Congrats! You Passed With A Score Of "+score+"%";
+    }else{
+      scoreText.innerHTML+="Sorry! You Didn't Pass! Your Score Is "+score+"%";
+    }
+    
   }
 
-
-  clickMe(id:string){
-    console.log("You clicked me" + id);
-  }
 
 }
