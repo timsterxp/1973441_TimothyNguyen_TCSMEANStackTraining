9297d3c932ce7e7c7f0cdc217543593b45b5bfb1
Tim Nguyen <timmyn97@gmail.com>
2021-04-16 15:15:32 -0700

    Complete add/delete/update, finalize fetch

--- MongoDB With Website/app.js
+++ MongoDB With Website/app.js
@@ -7,10 +7,18 @@
 // -Features: Add, Delete, Update and Fetch
 //==================================================
 
-
 let app = require("express")();
 let bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+//Database URL Details 
+let url = "mongodb://localhost:27017/meanstack";
+
+//middleware enable data from post method.
+app.use(bodyParser.urlencoded({ extended: true })); // enable body part data  
+app.use(bodyParser.json()); // json data. 
+
+
+
 /*
 index.html                  get 
 retreive all course         get
@@ -28,6 +36,16 @@ let CourseSchema = mongoose.Schema({
     amount: Number
 })
 
+//Database connection without warning 
+const mongooseDbOption = { // to avoid warning 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url, mongooseDbOption); //ready to connect 
+
+//Connect the data 
+mongoose.connection
+
 let CourseModel = mongoose.model("courses", CourseSchema, "Courses");
 
 app.get("/", (req, res) => {
@@ -42,10 +60,8 @@ app.get("/deleteCourse", (req, res) => {
 app.get("/updateCourse", (req, res) => {
     res.sendFile(__dirname + "/updateCourse.html");
 })
-app.get("/fetchCourse", (req, res) => {
-    res.sendFile(__dirname + "/fetchCourse.html");
-})
-app.post("/storeCDetails", (req, res) => {
+
+app.post("/addCourse", (req, res) => {
     /*
     retreive data from body part 
     connected to database 
@@ -60,23 +76,80 @@ app.post("/storeCDetails", (req, res) => {
         amount: req.body.amount
     });
 
-    console.log("Hello");
-})
-app.post("/deleteCDetails/:cid", (req, res) => {
-    /*
-    retreive data from body part 
-    connected to database 
-    store in database. 
-        res.sendFile(__dirname+"/index.html")
-    */
+    console.log(course);
+    course.save((err, result) => {
+            if (!err) {
+                res.send("Record saved")
+            } else {
+                res.send("Error saving")
+            }
+        })
+        //   res.sendFile(__dirname + "/index.html")
+
+
 })
 
-app.get("/get", (req, res) => {
+app.post("/deleteCourse", (req, res) => {
+    let cid = req.body.cid;
+    CourseModel.deleteOne({ _id: cid }, (err, result) => {
+            if (!err) {
+                if (result.deletedCount > 0) {
+                    res.send("Record deleted successfully")
+                } else {
+                    res.send("Record not present");
+                }
+            } else {
+                res.send("Error : " + err);
+            }
+        })
+        // res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/updateCourse", (req, res) => {
+    let cid = req.body.cid;
+    let updatePrice = req.body.amount;
+    let newDescription = req.body.description;
+    let newName = req.body.cname;
+    CourseModel.updateMany({ _id: cid }, { $set: { cname: newName, description: newDescription, amount: updatePrice } }, (err, result) => {
+            if (!err) {
+                if (result.deletedCount > 0) {
+                    res.send("Record deleted successfully")
+                } else {
+                    res.send("Record not present");
+                }
+            } else {
+                res.send("Error : " + err);
+            }
+        })
+        // res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/fetchCourse", (req, res) => {
     /*
         retrieve records from mongodb and store in array 
         
         res.json(arrayName);
 
     */
+
+    CourseModel.find({}, (err, result) => {
+
+        if (!err) {
+            let table = `<table>
+            <tr>
+                <th>Course Id</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>`;
+            //res.json(result);
+            for (course in result) {
+                console.log(result[course].cname);
+                table += `<tr><th>` + result[course]._id + `</th><th>` + result[course].cname + `</th><th>` + result[course].description + `</th><th>` + result[course].amount + `</th></tr>`;
+            }
+            res.write(table);
+        }
+    })
+
 })
 app.listen(9090, () => console.log("running.."));
