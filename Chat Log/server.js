d3f2ffd3fbeea591f3ab2484cae31a0366bbae3b
Tim Nguyen <timmyn97@gmail.com>
2021-04-19 09:48:19 -0700

    Set up Chat Log, use SocketIO Project as a base

---
+++ Chat Log/server.js
@@ -0,0 +1,27 @@
+//==================================================
+// Socket IO
+// TCS Simplilearn
+// Timothy Nguyen 1973441
+
+// Combine SocketIO With MongoDB capabilities
+//==================================================
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+//Access to HTML
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+//Initial Client Connection
+io.on("connection", (socket) => {
+    console.log("Client Connected To Application...");
+    //Socket IO Message Display
+    socket.on("chat", (msg) => {
+        console.log(msg);
+    })
+})
+
+// Port number can be changed as necessary
+http.listen(9090, () => console.log('server running on port number 9090'));
