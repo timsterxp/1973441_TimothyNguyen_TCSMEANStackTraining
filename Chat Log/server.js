fc8cfa3375903d2539a047faafd1f0293c0852d1
Tim Nguyen <timmyn97@gmail.com>
2021-04-19 11:12:30 -0700

    Successful save and retrieve of chats

--- Chat Log/server.js
+++ Chat Log/server.js
@@ -8,10 +8,47 @@
 let app = require("express")();
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
+let url = "mongodb://localhost:27017/meanstack";
+
+let mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+let ChatSchema = mongoose.Schema({
+    name: String,
+    message: String
+})
+
+//Database connection without warning 
+const mongooseDbOption = { // to avoid warning 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose.connect(url, mongooseDbOption); //ready to connect 
+
+//Connect the data 
+mongoose.connection
+
+
+let ChatModel = mongoose.model("chats", ChatSchema, "Chats");
 
 //Access to HTML
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/showLog", (req, res) => {
+    let tableFormat = "";
+    let data = ChatModel.find().select({
+        "name": 1,
+        "message": 1,
+        "_id": 0
+    });
+    //console.log(data);
+    data.exec(function(err, logging) {
+        if (err) console.log(err);
+
+        res.send(logging);
+    })
 })
 
 //Initial Client Connection
@@ -19,6 +56,15 @@ io.on("connection", (socket) => {
     console.log("Client Connected To Application...");
     //Socket IO Message Display
     socket.on("chat", (msg) => {
+        let chat = new ChatModel(msg);
+        chat.save((err, result) => {
+            if (!err) {
+                console.log("Chat successfully saved");
+            } else {
+                console.log(err);
+            }
+            //mongoose.disconnect();
+        })
         console.log(msg);
     })
 })
